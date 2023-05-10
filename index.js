3ce3ce7f028f8ea40c1aba392dfc3d35b1ed7ad6
Thiago <thiago_r_araujo@estudante.sesisenai.or.br>
2023-05-10 12:16:46 -0300

    coisando as versão

--- index.js
+++ index.js
@@ -14,7 +14,19 @@ app.use(express.static('public'))
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 // -----------------------------------
-
+app.get('/listar', async(req,res)=>{
+    const dados = await Usuario.findAll({raw:true})
+    console.log(dados)
+    console.log(dados.nome)
+    console.log(dados[0].nome)
+    console.log(dados[1].id)
+    // res.redirect('/')
+    res.render('lista', {valor: dados})
+})
+app.get('/', (req,res)=>{
+    // res.end('Teste de dados')
+    res.render('home')
+})
 // -----------------------------------
 conn.sync().then(()=>{
     app.listen(PORT, hostname, ()=>{
